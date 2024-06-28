63098a240c014f0cef8d72c866a19305287e2942
mdarafat2201 <maccenarafat@gmail.com>
2024-06-28 15:58:58 +0100

    banner part don1

--- src/HomeComponent/Banner/Banner.jsx
+++ src/HomeComponent/Banner/Banner.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import BannereImg from "../../assets/Bannerimg.jpeg"
+import BannereImg from "../../assets/Bannerimg.jpg"
 
 import { FaFacebookF,FaInstagram,FaWhatsapp,FaYoutube } from "react-icons/fa";
 const Banner = () => {
@@ -9,24 +9,24 @@ const Banner = () => {
     
   <div className=' relative w-full flex items-center justify-between h-[100vh] pt-16 px-[8%] bg-cover bg-center'>
      <div>
-        <h3 className='text-3xl font-bold text-main_bg_color'>Hello, Its Me</h3>
-        <h1 className='text-[46px] my-[-3px] font-bold text-main_bg_color'>ACCEN ARAFAT</h1>
-        <h3 className='text-3xl font-bold text-main_bg_color'>And Im a </h3>
-        <p className='text-[20px] font-normal text-main_bg_color'>Notes about your order, e.g. special notes for delivery. Notes about your order, e.g. special notes for delivery. </p>
-      <div className='flex items-center gap-x-5'>
+        <h3 className='text-3xl font-bold text-main_bg_color slistexttop'>Hello, Its Me</h3>
+        <h1 className='text-[46px] my-[-3px] font-bold text-main_bg_color slistextLeft'>ACCEN ARAFAT</h1>
+        <h3 className='text-3xl font-bold text-main_bg_color slistexttop'>And Im a </h3>
+        <p className='text-[20px] font-normal text-main_bg_color slistextRight'>Notes about your order, e.g. special notes for delivery. Notes about your order, e.g. special notes for delivery. </p>
+      <div className='flex items-center gap-x-5 slistextRight'>
          <a href="#" className='w-10 h-10 rounded-full border-2 border-[#0ef] flex justify-center items-center text-[20px] text-[#0ef] my-5 hover:bg-[#0ef] hover:text-[#000000]'><FaFacebookF/></a>
          <a href="#" className='w-10 h-10 rounded-full border-2 border-[#0ef] flex justify-center items-center text-[20px] text-[#0ef] my-5 hover:bg-[#0ef] hover:text-[#000000]'><FaInstagram/></a>
          <a href="#" className='w-10 h-10 rounded-full border-2 border-[#0ef] flex justify-center items-center text-[20px] text-[#0ef] my-5 hover:bg-[#0ef] hover:text-[#000000]'><FaWhatsapp/></a>
          <a href="#" className='w-10 h-10 rounded-full border-2 border-[#0ef] flex justify-center items-center text-[20px] text-[#0ef] my-5 hover:bg-[#0ef] hover:text-[#000000]'><FaYoutube/></a>
       </div>
-      <div>
+      <div className='slistexttop'>
         <button className='py-4 px-8 text-[20px] font-bold bg-[#0ef] border-2 border-[#0ef] hover:text-[#0ef] rounded-md hover:bg-transparent '>
               More About Me
         </button>
       </div>
     </div>
-    <div className='max-w-[600px]'>
-        <img src={BannereImg} alt={BannereImg} />
+    <div className='max-w-[600px] p-[100px] slistextRight'>
+        <img src={BannereImg} alt={BannereImg} className='w-full h-full object-cover rounded-full box_shodoe ' />
     </div>
     
 </div>
--- src/HomeComponent/Header/Header.jsx
+++ src/HomeComponent/Header/Header.jsx
@@ -5,24 +5,24 @@ const Header = () => {
     <div>
       <div className="container">
         <div className="flex items-center justify-between py-5 fixed top-0 left-0 z-10 w-full px-[8%] ">
-          <div>
-            <span className="text-2xl logo text-main_bg_color font-bold relative ">ARAFAT</span>
+          <div className="slistextLeft">
+            <span className="text-2xl text-main_bg_color font-bold relative ">ARAFAT</span>
           </div>
           <div>
             <ul className="flex items-center gap-x-5  ">
-              <li className="slistext text-main_bg_color text-2xl  font-normal ml-8 transition-all inline-block hover:text-[#0ef]">
+              <li className="slistextbottom text-main_bg_color text-2xl  font-normal ml-8 transition-all inline-block hover:text-[#0ef]">
                 <a href="#">Home</a>
               </li>
-              <li className="slistext text-main_bg_color text-2xl font-normal ml-8 transition-all inline-block hover:text-[#0ef">
+              <li className="slistextbottom text-main_bg_color text-2xl font-normal ml-8 transition-all inline-block hover:text-[#0ef]">
                 <a href="#">About</a>
               </li>
-              <li className="slistext text-main_bg_color text-2xl font-normal ml-8 transition-all inline-block hover:text-[#0ef">
+              <li className="slistextbottom text-main_bg_color text-2xl font-normal ml-8 transition-all inline-block hover:text-[#0ef]">
                 <a href="#">Skill</a>
               </li>
-              <li className="slistext text-main_bg_color text-2xl font-normal ml-8 transition-all inline-block hover:text-[#0ef">
+              <li className="slistextbottom text-main_bg_color text-2xl font-normal ml-8 transition-all inline-block hover:text-[#0ef]">
                 <a href="#">Potfolio</a>
               </li>
-              <li className="slistext text-main_bg_color text-2xl font-normal ml-8 transition-all inline-block hover:text-[#0ef">
+              <li className="slistextbottom text-main_bg_color text-2xl font-normal ml-8 transition-all inline-block hover:text-[#0ef]">
                 <a href="#">Contact</a>
               </li>
             </ul>
